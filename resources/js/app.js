b4cdea380984ba361c83fa59a9c27ab50d9b7085
xiaolynxx <duyrie910@gmail.com>
2022-12-27 18:10:01 +0700

    1

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+//import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 import Icon from './Components/Icon.vue';
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
@@ -16,7 +16,7 @@ createInertiaApp({
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
-            .use(ZiggyVue, Ziggy)
+            //.use(ZiggyVue, Ziggy)
             .component('icon', Icon)
             .mount(el);
     },
